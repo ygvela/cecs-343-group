a0030a61592a8e5412fdc011feed4bb0f11bbf1d
Yosselin <yvelasco598@gmail.com>
2019-06-25 16:32:25 -0700

    Illuminati code file

---
+++ Illuminati Game/src/classes/Illuminati.java
@@ -0,0 +1,17 @@
+package classes;
+import java.util.Scanner;
+
+public class Illuminati {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Welcome to Illuminati"
+				+ "1. Start Game"
+				+ "2. View Rules"
+				+ "3. End Game");
+		Scanner in = new Scanner(System.in);
+		int menu_choice = in.nextInt();
+
+	}
+
+}
