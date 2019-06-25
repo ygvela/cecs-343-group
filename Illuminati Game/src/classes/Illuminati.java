e6707219482d54efb8f06bdf60067d963a331d7c
Yosselin <yvelasco598@gmail.com>
2019-06-25 16:37:51 -0700

    added user input

--- Illuminati Game/src/classes/Illuminati.java
+++ Illuminati Game/src/classes/Illuminati.java
@@ -5,11 +5,13 @@ public class Illuminati {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Welcome to Illuminati"
-				+ "1. Start Game"
-				+ "2. View Rules"
-				+ "3. End Game");
+		System.out.print("Welcome to Illuminati\n"
+				+ "\n1. Start Game"
+				+ "\n2. View Rules"
+				+ "\n3. End Game\n");
+		
 		Scanner in = new Scanner(System.in);
+		System.out.print("\nEnter a number: ");
 		int menu_choice = in.nextInt();
 
 	}
