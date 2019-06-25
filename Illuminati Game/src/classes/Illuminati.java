6891680647aa01b45976c8ded8dbab24bfe55f6f
Yosselin <yvelasco598@gmail.com>
2019-06-25 16:57:17 -0700

    added input validation

--- Illuminati Game/src/classes/Illuminati.java
+++ Illuminati Game/src/classes/Illuminati.java
@@ -3,17 +3,35 @@ import java.util.Scanner;
 
 public class Illuminati {
 
-	public static void main(String[] args) {
-		
-		System.out.print("Welcome to Illuminati\n"
-				+ "\n1. Start Game"
+	public static int check_input(int lower,int upper) {
+		Scanner in = new Scanner(System.in);
+		int input = 0;
+		boolean invalid = true;
+		while (invalid){
+			System.out.print("\nSelect a menu option: ");
+			input = in.nextInt();
+			if(input >= lower && input <= upper) {
+				invalid = false;
+			}else {
+				System.out.print("Invalid Range.");
+			}
+		}
+		return input;
+	}
+	
+	public static void display_start_menu() {
+		System.out.print("\n1. Start Game"
 				+ "\n2. View Rules"
 				+ "\n3. End Game\n");
-		
-		Scanner in = new Scanner(System.in);
-		System.out.print("\nEnter a number: ");
-		int menu_choice = in.nextInt();
-
 	}
-
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int menu_num = 0;
+		System.out.print("Welcome to Illuminati\n");
+		while(menu_num != 3) {
+			display_start_menu();
+			menu_num = check_input(1,3);
+		}
+	}
 }
