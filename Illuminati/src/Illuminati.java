ca5747d21ac7cf074f45f16ccdda15d0d55157ab
miche <miche@10.39.109.148>
2019-06-26 16:32:01 -0700

    updated check_input function and added numplayers check

--- Illuminati/src/Illuminati.java
+++ Illuminati/src/Illuminati.java
@@ -1,18 +1,39 @@
 import java.util.Scanner;
 
 public class Illuminati {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Welcome to Illuminati: A Game of Conspiracy\n");
+		display_start_menu();
+		int menu_num = check_input(1, 3);
+		if(menu_num == 1) {
+			System.out.print("\nStarting Game...");
+			System.out.print("\nEnter the number of players (2-8): ");
+			int num_players = check_input(2, 8);
+		}
+		if(menu_num == 2) {
+			System.out.print("\nViewing Rules...");
+		}
+	}
 
 	public static int check_input(int lower,int upper) {
 		Scanner in = new Scanner(System.in);
 		int input = 0;
-		boolean invalid = true;
-		while (invalid){
-			System.out.print("\nSelect a menu option: ");
-			input = in.nextInt();
-			if(input >= lower && input <= upper) {
-				invalid = false;
-			}else {
-				System.out.print("Invalid Range.");
+		boolean isValid = false;
+		while(!isValid) {
+			if(in.hasNextInt()) {
+				input = in.nextInt();
+				if(input >= lower && input <= upper) {
+					isValid = true;
+				} else {
+					in.nextLine();
+					System.out.print("Invalid range. Please enter a number between " +
+					lower + " and " + upper + ": ");
+				}
+			} else {
+				in.nextLine();
+				System.out.print("Invalid option. Please enter a number: ");
 			}
 		}
 		return input;
@@ -22,19 +43,5 @@ public class Illuminati {
 		System.out.print("\n1. Start Game"
 				+ "\n2. View Rules"
 				+ "\n3. End Game\n");
-	}
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int menu_num = 0;
-		System.out.print("Welcome to Illuminati: A Game of Conspiracy\n");
-		display_start_menu();
-		menu_num = check_input(1,3);
-		if(menu_num == 1) {
-			System.out.print("\nStarting Game...");
-		}
-		if(menu_num == 2) {
-			System.out.print("\nViewing Rules...");
-		}
 	}
 }
