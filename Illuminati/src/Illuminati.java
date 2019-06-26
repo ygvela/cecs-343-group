81dcc66d170609fdf85b830f4b122ec06e27ca30
Yosselin <yvelasco598@gmail.com>
2019-06-26 16:32:07 -0700

    added Menu class

--- Illuminati/src/Illuminati.java
+++ Illuminati/src/Illuminati.java
@@ -18,17 +18,12 @@ public class Illuminati {
 		return input;
 	}
 	
-	public static void display_start_menu() {
-		System.out.print("\n1. Start Game"
-				+ "\n2. View Rules"
-				+ "\n3. End Game\n");
-	}
-	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int menu_num = 0;
 		System.out.print("Welcome to Illuminati: A Game of Conspiracy\n");
-		display_start_menu();
+		Menu menu = new Menu();
+		menu.display_start_menu();
 		menu_num = check_input(1,3);
 		if(menu_num == 1) {
 			System.out.print("\nStarting Game...");
---
+++ Illuminati/src/Menu.java
@@ -0,0 +1,10 @@
+
+public class Menu {
+
+	public void display_start_menu() {
+		System.out.print("\n1. Start Game"
+				+ "\n2. View Rules"
+				+ "\n3. End Game\n");
+	}
+	
+}
