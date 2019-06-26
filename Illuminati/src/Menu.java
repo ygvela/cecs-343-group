cba5cd47cb4179f4443ff614cf00d0552f5177a5
Yosselin <yvelasco598@gmail.com>
2019-06-26 16:44:27 -0700

    updated Menu class

--- Illuminati/src/Menu.java
+++ Illuminati/src/Menu.java
@@ -4,7 +4,8 @@ public class Menu {
 	public void display_start_menu() {
 		System.out.print("\n1. Start Game"
 				+ "\n2. View Rules"
-				+ "\n3. End Game\n");
+				+ "\n3. End Game\n"
+				+ "\nEnter a number: ");
 	}
 	
 }
